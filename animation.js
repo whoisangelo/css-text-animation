35bc2e9d928dbd64a41a07b03ba3d1a1fa3796ac
Angelo Vitor <angelo.vitor1998@hotmail.com>
2019-03-11 13:09:44 -0400

    First commit

---
+++ animation.js
@@ -0,0 +1,9 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+typeWriter(document.querySelector('h1'));
